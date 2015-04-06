a7ec22683b6f73639f54059bbef54a1e5c774638
jvvlives2005 <jvvlives2005@aol.com>
2015-04-05 20:07:10 -0400

    update from team review

--- src/nyc/c4q/jvvlives2005/DistributionCalculator.java
+++ src/nyc/c4q/jvvlives2005/DistributionCalculator.java
@@ -1,8 +1,10 @@
 package nyc.c4q.jvvlives2005;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 /**
@@ -25,8 +27,52 @@ import java.util.Scanner;
 
 public class DistributionCalculator
 {
-    public static File calculate(File textFile){
-        //inout a textfile
+    public static ArrayList<Double> calculate(File textFile) throws FileNotFoundException{
+
+     ArrayList<Double> percentages = new ArrayList<Double>();
+
+     int totalNumberOfLetters = 0;
+
+     HashMap<Character, Integer> percentageTable = new HashMap<Character, Integer>();
+
+     for(int i = 0; i < 26; i++) {
+      percentageTable.put((char)(i+97),0);
+     }
+
+     Scanner input = new Scanner(textFile);
+     String word;
+     while(input.hasNext())
+     {
+      word = input.next();
+
+      for(int i = 0; i < word.length(); i++)
+      {
+       char c = word.toLowerCase().charAt(i);
+
+
+      //gets only the letters; not spaces or " or ?
+       if (Character.isLetter(c)){
+         percentageTable.put(c,percentageTable.get(c)+1);
+        totalNumberOfLetters ++;
+       }
+      }
+     }
+
+
+     for (int count : percentageTable.values()) {
+      double percentage =  (count*100)/totalNumberOfLetters;
+      percentages.add(percentage);
+     }
+     return percentages;
+    }
+
+ public static void main(String[] args) throws FileNotFoundException{
+  File file = new File ("/Users/c4q-joshelynvivas/Desktop/AccessCode/HW-20150403/src/nyc/c4q/jvvlives2005/Alphabet.txt");
+  ArrayList<Double> list = calculate(file);
+ }
+
+
+ //inout a textfile
     //}
 
     //an array list is made like ArrayList<Percent> alphabets
